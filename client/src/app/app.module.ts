22ee3482ea5b7ba04ce42e42197ff8896711d52a
aimenAlt <work@Aimens-MacBook-Pro-2.local>
2021-11-27 13:30:19 -0800

    Last working version

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,17 +5,11 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './users/login/login.component';
 import { AppRoutingModule } from './app-routing.module';
 import {FormsModule} from "@angular/forms";
-import { HomeEmployeeComponent } from './home-employee/home-employee.component';
-import { HomeManagerComponent } from './home-manager/home-manager.component';
-import { SubmitPageComponent } from './employee/submit-page/submit-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    HomeEmployeeComponent,
-    HomeManagerComponent,
-    SubmitPageComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
