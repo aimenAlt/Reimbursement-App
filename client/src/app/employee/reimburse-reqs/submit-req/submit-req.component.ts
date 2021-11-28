e7c0b4e408456f7c64cb727517d368fb162b86f9
aimenAlt <work@Aimens-MacBook-Pro-2.local>
2021-11-27 16:55:54 -0800

    add reimburse component and services

---
+++ client/src/app/employee/reimburse-reqs/submit-req/submit-req.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-submit-req',
+  templateUrl: './submit-req.component.html',
+  styleUrls: ['./submit-req.component.css']
+})
+export class SubmitReqComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ client/src/app/reimburse-reqs/request.model.ts
@@ -0,0 +1,7 @@
+export class Request {
+  id: number = 0;
+  description: string = '';
+  cost: number = 0.00;
+  date: string = '';
+  imageLink: string = '';
+}
